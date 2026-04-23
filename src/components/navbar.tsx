ee2a607e8178f5342aea82cf3b0616e524dd6c92
sayandweep <sayandweepbiswas@gmail.com>
2026-04-23 17:53:36 +0530

    23426 1753

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,8 +1,6 @@
 import {
   NavigationMenu,
-  NavigationMenuContent,
   NavigationMenuItem,
-  NavigationMenuLink,
   NavigationMenuList,
   NavigationMenuTrigger,
 } from "@/components/ui/navigation-menu"
